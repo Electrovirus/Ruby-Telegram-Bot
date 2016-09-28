af58b26534fa604d73aa21f554a490d57a563d9d
Electrovirus <E.electrovirus@gmail.com>
2016-09-28 19:54:52 +0330

    Update self.rb

--- plugins/self.rb
+++ plugins/self.rb
@@ -5,7 +5,7 @@
 	case message.text
 	when "/add"
 		@config["Groups"].insert(0,message.chat.id)
-		bot.api.send_message(chat_id: message.chat.id, text: "تم التفعيل!" )
+		bot.api.send_message(chat_id: message.chat.id, text: "Bot activated 🔋" )
 	when "/prom"
 		@config["Admins"].insert(0,message.reply_to_message.from.id)
 		bot.api.send_message(chat_id: message.chat.id, text: "#{message.reply_to_message.from.first_name} #{message.reply_to_message.from.last_name} has been promoted" )
@@ -13,9 +13,9 @@
 		bot.api.send_message(chat_id: message.chat.id, text: "#{message.reply_to_message.from.first_name} #{message.reply_to_message.from.last_name} has been disqualified" )
 	end
  end
-  s_tofiko = "عزيزي🐸🌸 \n: ممنوع ارسال االملصقات هنا ستتعرض للطرد!"
+  s_tofiko = "Please do not send stickers🐼❎"
  h_tofiko = "  ممنوع ارسال الروابط❌"
-  v_tofiko = "عزيزي🐸🌸: \n ممنوع ارسال الوسائط هنا ستتعرض للطرد!❌"
+  v_tofiko = "Please do not send videos🐼❎"
   w_tofiko =  "اهلا بك \n  تابع @mtofee" 
 if @config["Admins"].include?(message.from.id)
 	
@@ -38,24 +38,24 @@ if message.text.to_s.include?"http"
 end
 if  message.text == "!طرد" && message.reply_to_message && @config["Admins"].include?(message.from.id)
     bot.api.kickChatMember(chat_id: message.chat.id, user_id: message.reply_to_message.from.id)
-    bot.api.send_message(chat_id: message.chat.id, text: "لقد تم الطرد من المجموعة!" )
+    bot.api.send_message(chat_id: message.chat.id, text: "Kicked from the group🔴" )
 end
 
 case message.text
 
   when "/start"
 if db[message.from.id]
-      bot.api.send_message(chat_id: message.chat.id, text: " تابع @mtofee!" )
+      bot.api.send_message(chat_id: message.chat.id, text: " Hi welcome to my ruby bot🌎" )
     else
 			db[message.from.id] = {
 				"username"=>message.from.username,
 }
-							bot.api.send_message(chat_id: message.chat.id, text: "تابع @mtofee  !" )
+							bot.api.send_message(chat_id: message.chat.id, text: "Created by @Electrovirus" )
 			puts "#{message.from.username}".on_green
 end
 	when "/me"
     if db[message.from.id]
-      bot.api.send_message(chat_id: message.chat.id, text: 	"معرفك: @#{message.from.username} \n الأيدي الخاص بك: #{message.from.id} ")
+      bot.api.send_message(chat_id: message.chat.id, text: 	"Username: @#{message.from.username} \n Id: #{message.from.id} ")
     end
 if message.new_chat_member
    bot.api.send_message(chat_id: message.chat.id, text: w_tofiko)
@@ -68,7 +68,7 @@ end
   end
     if message.text.to_s.include?("http")
     bot.api.kickChatMember(chat_id: message.chat.id, user_id: message.from.id)
-    bot.api.send_message(chat_id: message.chat.id, text: "لقد تم الطرد من المجموعة!" )
+    bot.api.send_message(chat_id: message.chat.id, text: "Kicked from the group" )
     end
     if message
     	bot.api.send_message(chat_id: message.chat.id, text : "قناتي https://telegram.me/joinchat/Bv5V_0EOlVkxaj7lNNqGFg")
